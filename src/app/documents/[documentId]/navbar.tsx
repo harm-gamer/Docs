d499fa47cf4d9cca2a032f3e5df3f358f8062cc4
harm-gamer <mukundt513@gmail.com>
2025-01-22 01:41:00 +0530

    Added tags functionality

--- src/app/documents/[documentId]/navbar.tsx
+++ src/app/documents/[documentId]/navbar.tsx
@@ -20,6 +20,7 @@ import { BsFilePdf } from "react-icons/bs"
 import { useEditorStore } from "@/store/use-editor-store"
 import { escape } from "querystring"
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs"
+import { Avatars } from "./avatars"
 export const Navbar = () =>{
 
     const {editor} = useEditorStore();
@@ -208,6 +209,7 @@ export const Navbar = () =>{
           </div>
           </div>
           <div className="flex gap-3 items-center pl-6">
+            <Avatars/>
     <OrganizationSwitcher
     afterCreateOrganizationUrl="/"
     afterLeaveOrganizationUrl="/"
--- src/app/documents/[documentId]/room.tsx
+++ src/app/documents/[documentId]/room.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useMemo, useState } from "react";
 import {
   LiveblocksProvider,
   RoomProvider,
@@ -8,13 +8,44 @@ import {
 } from "@liveblocks/react/suspense";
 import { useParams } from "next/navigation";
 import { FullscreenLoader } from "@/components/fullscreen-loader";
+import { toast } from "sonner";
+import { getUsers } from "./actions";
 
+type User = {id : string; name : string; avatar : string}
 export function Room({ children }: { children: ReactNode }) {
     const params = useParams();
+
+    const [users,setUsers] = useState<User[]>([]);
+      const fetchUsers = useMemo(() => async () => {
+        try{
+          const list  = await getUsers()
+        }catch{
+          toast.error("Failed to fetch users")
+        }
+      },[])
+
+      useEffect(() => {
+        fetchUsers();
+      },[fetchUsers])
+
   return (
     <LiveblocksProvider 
       throttle={16}
       authEndpoint="/api/liveblocks-auth"
+      resolveUsers={({userIds}) => {
+        return userIds.map((userId) => users.find((user) => user.id == userId)?? undefined )
+      }}
+      resolveMentionSuggestions={({text}) => {
+        let filterUsers = users;
+
+        if(text){
+          filterUsers = users.filter((user) =>
+            user.name.toLowerCase().includes(text.toLowerCase())
+          );
+        }
+        return filterUsers.map((user) => user.id)
+      }}
+      resolveRoomsInfo={() => []}
     >
       <RoomProvider id={params.documentId as string}>
         <ClientSideSuspense fallback={<FullscreenLoader label="Room loading..."/>}>
